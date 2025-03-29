a1979892f896ebd06de291cc9a6a7b035f716a29
Nils Langner <nils.langner@startwind.io>
2025-03-29 13:27:30 +0100

    affiliate

--- functions.php
+++ functions.php
@@ -1,7 +1,19 @@
 <?php
 
 function mein_custom_styles() {
-	wp_enqueue_style('custom-style', get_stylesheet_directory_uri() . '/style.css', array(), '1.0', 'all');
+	wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/style.css', array(), '1.0', 'all' );
 }
 
-add_action('wp_enqueue_scripts', 'mein_custom_styles');
+add_action( 'wp_enqueue_scripts', 'mein_custom_styles' );
+
+function acf_affiliate_shortcode() {
+	$affiliate = get_field( 'affiliate' );
+
+	if ( $affiliate ) {
+		return $affiliate;
+	} else {
+		return "";
+	}
+}
+
+add_shortcode( 'affiliate_link', 'acf_affiliate_shortcode' );
